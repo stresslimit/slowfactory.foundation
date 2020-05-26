245341c3d0768cf778fcdd5f7859de50d9902f78
stresslimit <colin.vernon@gmail.com>
2020-05-26 17:09:48 -0400

    add cardstack js framework

--- _js/index.js
+++ _js/index.js
@@ -1,9 +1,14 @@
-import initReactFastclick from 'react-fastclick';
+import initReactFastclick from 'react-fastclick'
+import cardstack from './cardstack'
 import { init as mobile_menu, close_mobile_menu } from './mobile-menu'
 
 window.onload = () => {
 
   initReactFastclick()
   mobile_menu()
+
+  let card = document.querySelector('.card')
+  if ( card )
+    cardstack(card)
 
 }
