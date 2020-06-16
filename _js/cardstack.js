b182301a71d99594a67f2566c12201d6a25df474
ciennafern <ciennafern@gmail.com>
2020-06-16 09:57:09 -0700

    pulled arrows out of first card

--- _js/cardstack.js
+++ _js/cardstack.js
@@ -40,5 +40,14 @@ export default (cardSelector) => {
   	setTimeout(function(){appendToList()}, 150)
   })
 
+  window.addEventListener('load', function(){
+    var allimages= document.getElementsByTagName('img');
+    for (var i=0; i<allimages.length; i++) {
+        if (allimages[i].getAttribute('data-src')) {
+           allimages[i].setAttribute('src', allimages[i].getAttribute('data-src'));
+        }
+    }
+})
+
 
 }
--- _js/lazyload.js
+++ _js/lazyload.js
@@ -1,10 +1,9 @@
-<script>
+ $('.next').click(function(e){
 window.addEventListener('load', function(){
     var allimages= document.getElementsByTagName('img');
     for (var i=0; i<allimages.length; i++) {
         if (allimages[i].getAttribute('data-src')) {
-            allimages[i].setAttribute('src', allimages[i].getAttribute('data-src'));
         }
     }
 }, false)
-</script>
+}
