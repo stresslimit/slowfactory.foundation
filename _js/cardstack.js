df2755295bec7bbd4e9530d2008904670587aa5d
ciennafern <ciennafern@gmail.com>
2020-06-18 15:40:54 -0700

    add slick file to js

--- _js/cardstack.js
+++ _js/cardstack.js
@@ -1,6 +1,7 @@
 export default (cardSelector) => {
 
   let cardList = document.querySelector('.card-list')
+  // let cards = document.querySelectorAll('.card-list .card')
   if ( !cardList ) return
 
   var $card = $('.card')
@@ -13,39 +14,30 @@ export default (cardSelector) => {
   function init() {
     let lastChild = $cards.last()
     lastChild.addClass('activeNow')
-    resetImages()
   }
 
   $('.next').click(function(e){
     e.preventDefault()
-  	var prependList = function() {
-			var $slicedCard = $('.card-list .card').slice(lastCardIndex).removeClass('transformThis activeNow')
-			$('.card-list').prepend($slicedCard)
-      resetImages()
-  	}
-    let $newActive = $('.card').last()
-  	$newActive.removeClass('transformPrev').addClass('transformThis').prev().addClass('activeNow')
-  	setTimeout(function(){ prependList() }, 150)
+    var prependList = function() {
+      if( $('.card-list .card').hasClass('activeNow') ) {
+        var $slicedCard = $('.card-list .card').slice(lastCardIndex).removeClass('transformThis activeNow')
+        $('.card-list').prepend($slicedCard)
+      }
+    }
+    $('li').last().removeClass('transformPrev').addClass('transformThis').prev().addClass('activeNow')
+    setTimeout(function(){ prependList() }, 150)
   })
 
   $('.prev').click(function(e) {
     e.preventDefault()
-  	var appendToList = function() {
-			var $slicedCard = $('.card-list .card').slice(0, 1).addClass('transformPrev')
-			$('.card-list').append($slicedCard)
-      resetImages()
+    var appendToList = function() {
+      if( $('.card').hasClass('activeNow') ) {
+        var $slicedCard = $('.card').slice(0, 1).addClass('transformPrev')
+        $('.card-list').append($slicedCard)
+      }
     }
-		$('.card').removeClass('transformPrev').last().addClass('activeNow').prevAll().removeClass('activeNow')
-  	setTimeout(function(){ appendToList() }, 150)
+    $('li').removeClass('transformPrev').last().addClass('activeNow').prevAll().removeClass('activeNow')
+    setTimeout(function(){appendToList()}, 150)
   })
 
-}
-
-function resetImages() {
-  let $el = $('.card-list .card').last()
-  $el.find('img').each(function (i) {
-    console.log('resetting', $el)
-    let s = $(this).data('src')
-    $(this).attr('src', s)
-  })
 }
